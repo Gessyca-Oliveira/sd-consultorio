5da574475d0b1db425c09044c0f5feddb0d813b7
Gessyca-Oliveira <gessyca.estudos@gmail.com>
2026-04-15 18:14:18 -0300

    feat: define POJO classes for patient and consultation services

---
+++ pojos/consulta.py
@@ -0,0 +1,5 @@
+class Consulta:
+    def __init__(self, tipo: str, valor: float, forma_pagamento: str):
+        self.tipo = tipo
+        self.valor = valor
+        self.forma_pagamento = forma_pagamento
---
+++ pojos/paciente.py
@@ -0,0 +1,5 @@
+class Paciente:
+    def __init__(self, id_animal: int, nome: str, especie: str):
+        self.id_animal = id_animal
+        self.nome = nome
+        self.especie = especie
